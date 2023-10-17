e9256e442e387ec592e77580535145d5e2a88e79
Williams25 <william.souza.timao@hotmail.com>
2023-10-17 19:05:58 -0300

    desafio Nomenclatura de variáveis

--- desafios/01-nomenclatura-de-variaveis.ts
+++ desafios/01-nomenclatura-de-variaveis.ts
@@ -1,55 +1,64 @@
 // Nomenclatura de variáveis
 
-const list = [
+const categoryList = [
   {
-    title: 'User',
-    followers: 5
+    title: "User",
+    followers: 5,
   },
   {
-    title: 'Friendly',
+    title: "Friendly",
     followers: 50,
   },
   {
-    title: 'Famous',
+    title: "Famous",
     followers: 500,
   },
   {
-    title: 'Super Star',
+    title: "Super Star",
     followers: 1000,
   },
-]
+];
 
-export default async function getData(req, res) {
-  const github = String(req.query.username)
+export default async function getGithubCategory(req, res) {
+  const userName = String(req.query.username);
 
-  if (!github) {
+  if (!userName) {
     return res.status(400).json({
-      message: `Please provide an username to search on the github API`
-    })
+      message: `Please provide an username to search on the github API`,
+    });
   }
 
-  const response = await fetch(`https://api.github.com/users/${github}`);
+  const gitHubUser = await fetch(`https://api.github.com/users/${userName}`);
 
-  if (response.status === 404) {
+  if (gitHubUser.status === 404) {
     return res.status(400).json({
-      message: `User with username "${github}" not found`
-    })
+      message: `User with username "${userName}" not found`,
+    });
   }
 
-  const data = await response.json()
+  const gitHubUserData = await gitHubUser.json();
 
-  const orderList = list.sort((a, b) =>  b.followers - a.followers); 
+  const orderByCategory = categoryList.sort(
+    (a, b) => b.followers - a.followers
+  );
 
-  const category = orderList.find(i => data.followers > i.followers)
+  const category = orderByCategory.find(
+    (i) => gitHubUserData.followers > i.followers
+  );
 
   const result = {
-    github,
-    category: category.title
-  }
+    userName,
+    category: category.title,
+  };
 
-  return result
+  return result;
 }
 
-getData({ query: {
-  username: 'josepholiveira'
-}}, {})
+getGithubCategory(
+  {
+    query: {
+      username: "josepholiveira",
+    },
+  },
+  {}
+);
