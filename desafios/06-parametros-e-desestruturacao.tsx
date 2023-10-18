0c4274657954413dc97c307d39433fef30535cba
Williams25 <william.souza.timao@hotmail.com>
2023-10-18 16:35:50 -0300

    Parâmetros e desestruturação

--- desafios/06-parametros-e-desestruturacao.tsx
+++ desafios/06-parametros-e-desestruturacao.tsx
@@ -1,11 +1,33 @@
-function updateUserRoute(body, params) {
-  updateUserController(body, params)
+type UpdateRepository = { name: string; email: string; password: string };
+type Params = { id: string };
+
+function updateUserRoute(body: UpdateRepository, params: Params) {
+  const { name, email, password } = body;
+  const { id } = params;
+  updateUserController({
+    name,
+    email,
+    password,
+    id,
+  });
 }
 
-function updateUserController(data, params) {
-  userRepository.update(data, params)
+function updateUserController({
+  email,
+  name,
+  password,
+  id,
+}: UpdateRepository & Params) {
+  userRepository.update({
+    email,
+    name,
+    password,
+    id,
+  });
 }
 
 const userRepository = {
-  update: (data, params) => {},
-}
+  update: ({ email, name, password, id }: UpdateRepository & Params) => {
+    console.log({ email, name, password, id });
+  },
+};
