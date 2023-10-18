4ffd7212667b185ae8246ad86cf1e3e409990451
Williams25 <william.souza.timao@hotmail.com>
2023-10-18 17:38:51 -0300

    syntatic sugars

--- desafios/07-numeros-magicos.js
+++ desafios/07-numeros-magicos.js
@@ -4,4 +4,4 @@ const MINUTES = 1000 * 60 * 30; // 30 minutes
 
 setInterval(lookForUpdades, MINUTES);
 
-function calculateDiscount(price, discount) {}
+function calculateDiscount(priceInCents, discountInPercentage) {}
